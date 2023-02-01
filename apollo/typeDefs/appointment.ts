abd0baaf30063bf7c6a310470dcab8db12c69545
Ngoran Aristide <ngoranaristide4@gmail.com>
2023-02-01 16:03:02 +0100

    Testimonies Crud implementation

--- apollo/typeDefs/appointment.ts
+++ apollo/typeDefs/appointment.ts
@@ -6,35 +6,99 @@ export const typeDefs = gql`
   scalar Date
 
   type Appointment {
-    id: String
-    firstName: String
-    lastName: String
-    email: String
-    phone: String
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    phone: String!
+    date: Date
     createdAt: Date
     updatedAt: Date
+    country: String!
+    # requestStatus: Status
+  }
+
+  enum Status {
+    PENDING
+    APPROVED
+  }
+
+  input CreateAppointmentInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    phone: String!
     date: Date
-    country: String
+    country: String!
+    # requestStatus: String
   }
 
   extend type Query {
+    "Get all appointments"
     appointments: [Appointment]
-    appointment(id: String!): Appointment
+    "Get a single appointment"
+    appointment(id: ID!): Appointment
+  }
+
+  "The response from the creation of an appointment"
+  type CreateAppointmentResponseType {
+    code: String!
+    success: Boolean!
+    message: String!
+    appointment: Appointment
+  }
+
+  type Mutation {
+    "This is the creation of an appointment"
+    createAppointment(
+      content: CreateAppointmentInput!
+    ): CreateAppointmentResponseType!
   }
 `;
 
 export const appointmentResolvers = {
   Query: {
-    appointments(_: any, __: any, context: ServerContext) {
-      return context.prisma.appointment.findMany();
+    async appointments(_: any, __: any, context: ServerContext) {
+      return await context.prisma.appointment.findMany();
     },
 
-    appointment(_: any, args: any, ctx: ServerContext) {
-      return ctx.prisma.appointment.findUnique({
+    async appointment(_: any, args: any, ctx: ServerContext) {
+      return await ctx.prisma.appointment.findUnique({
         where: {
           id: args.id,
         },
       });
+    },
+  },
+  Mutation: {
+    createAppointment: async (_: any, args: any, ctx: ServerContext) => {
+      // console.log("Date", new Date().getUTCDate());
+
+      try {
+        const appointment = await ctx.prisma.appointment.create({
+          data: {
+            ...args.content,
+            // requestStatus: "PENDING",
+          },
+        });
+
+        return {
+          code: 200,
+          success: true,
+          message:
+            "Your appointment was successfully booked. We will reach out to you shortly!",
+          appointment,
+        };
+      } catch (error: any) {
+        console.log("erroorrrrrr", error);
+
+        return {
+          code: error?.code,
+          success: false,
+          message: error?.meta?.target,
+          appointment: null,
+        };
+      }
     },
   },
 };
--- apollo/typeDefs/testimony.ts
+++ apollo/typeDefs/testimony.ts
@@ -5,7 +5,7 @@ export const typeDefs = gql`
   #graphql
 
   type Testimony {
-    id: String
+    id: ID!
     name: String
     country: String
     testimony: String
@@ -14,7 +14,7 @@ export const typeDefs = gql`
 
   extend type Query {
     testimonies: [Testimony]
-    testimony(id: String!): Testimony
+    testimony(id: ID!): Testimony
   }
 
   input TestimonyInput {
@@ -24,19 +24,11 @@ export const typeDefs = gql`
     published: Boolean
   }
 
-  input TestimonyPublishInput {
-    published: Boolean
-  }
-
   type Mutation {
     addTestimony(content: TestimonyInput!): Testimony!
-    editTestimony(id: String!, content: TestimonyInput!): Testimony!
-    editTestimonyState(
-      id: String!
-      content: TestimonyInput!
-      published: TestimonyPublishInput!
-    ): Testimony!
-    deleteTestimony(id: String): Testimony!
+    editTestimony(id: ID!, content: TestimonyInput!): Testimony!
+    editTestimonyState(id: ID!): Testimony!
+    deleteTestimony(id: ID!): Testimony!
   }
 `;
 
@@ -56,21 +48,7 @@ export const resolver = {
   Mutation: {
     async addTestimony(_: any, args: any, ctx: ServerContext) {
       console.log(args);
-      return ctx.prisma.testimony.create({
-        data: {
-          name: args.content.name,
-          country: args.content.country,
-          testimony: args.content.content,
-          published: false,
-        },
-      });
-    },
-    async editTestimony(_: any, args: any, ctx: ServerContext) {
-      // handle the case for drafts here first before anything
-      return ctx.prisma.testimony.update({
-        where: {
-          id: args.id,
-        },
+      return await ctx.prisma.testimony.create({
         data: {
           name: args.content.name,
           country: args.content.country,
@@ -79,19 +57,33 @@ export const resolver = {
         },
       });
     },
-
-    async editTestimonyState(_: any, args: any, ctx: ServerContext) {
-      return ctx.prisma.testimony.update({
-        where: { id: args.id },
+    async editTestimony(_: any, args: any, ctx: ServerContext) {
+      // handle the case for drafts here first before anything
+      return await ctx.prisma.testimony.update({
+        where: {
+          id: args.id,
+        },
         data: {
           ...args.content,
-          published: args.published,
+          published: false,
+        },
+      });
+    },
+
+    async editTestimonyState(_: any, args: any, ctx: ServerContext) {
+      // const testimony = await ctx.prisma.testimony.findUnique({
+      //   where: { id: args.id },
+      // });
+      return await ctx.prisma.testimony.update({
+        where: { id: args.id },
+        data: {
+          published: true,
         },
       });
     },
 
     async deleteTestimony(_: any, args: any, ctx: ServerContext) {
-      return ctx.prisma.testimony.delete({
+      return await ctx.prisma.testimony.delete({
         where: { id: args.id },
       });
     },
